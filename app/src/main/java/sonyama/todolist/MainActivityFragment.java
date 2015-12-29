3b8b6b79aba1f5a1753027f12834e23d380456af
ngoduyson <ngo.duy.son@framgia.com>
2015-12-29 19:27:30 +0700

    demo

--- app/src/main/java/sonyama/todolist/MainActivityFragment.java
+++ app/src/main/java/sonyama/todolist/MainActivityFragment.java
@@ -5,11 +5,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
 
     public MainActivityFragment() {
     }
@@ -17,6 +24,7 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+
     }
 }
